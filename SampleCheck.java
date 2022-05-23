4b67f54cf6994dbee9292b725660ee96679d09d6
obsqurazone11 <106041304+obsqurazone11@users.noreply.github.com>
2022-05-23 20:58:07 +0530

    My new project upload

---
+++ SampleCheck.java
@@ -0,0 +1,15 @@
+package sample.program;
+
+public class SampleCheck {
+	public void display()
+	{
+		System.out.println("HELLO WORLD");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+SampleCheck ob=new SampleCheck();
+ob.display();
+	}
+
+}
